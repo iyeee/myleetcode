1154991e14c8f3d6822b54dff95a69f1a1126da4
iyeee <1009699416@qq.com>
2021-09-18 18:23:06 +0800

    p

---
+++ 28.实现-str-str.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode.cn id=28 lang=python3
+#
+# [28] 实现 strStr()
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle)==0:
+            return 0
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)]==needle:
+                return i
+        return -1
+# @lc code=end
+# solution1:暴力解法
+# 最后一个case超时
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if len(needle)==0:
+#             return 0
+#         lenth1=len(haystack)
+#         lenth2=len(needle)
+#         for i in range(lenth1-lenth2+1):
+#             flag=True
+#             for j in range(lenth2):
+#                 if haystack[i+j]!=needle[j]:
+#                     flag=False
+#                     break
+#             if flag:
+#                 return i
+#         return -1
+
+# solution2: ac
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if len(needle)==0:
+#             return 0
+#         for i in range(len(haystack)):
+#             if haystack[i:i+len(needle)]==needle:
+#                 return i
+#         return -1
+
+
+
+# solution3: kmp
+# to do
