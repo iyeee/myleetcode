2acf8993c5daedf1bad1deaf56fa3e9e67fc1145
iyeee <1009699416@qq.com>
2021-09-18 18:28:42 +0800

    m

--- 28.实现-str-str.py
+++ 28.实现-str-str.py
@@ -9,7 +9,7 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle)==0:
             return 0
-        for i in range(len(haystack)):
+        for i in range(len(haystack)-len(needle)+1):
             if haystack[i:i+len(needle)]==needle:
                 return i
         return -1
@@ -46,3 +46,4 @@ class Solution:
 
 # solution3: kmp
 # to do
+
