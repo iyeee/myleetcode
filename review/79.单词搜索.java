0d79e8c0002f735f6c78c1a0259db0b27a519f4d
iyeee <1009699416@qq.com>
2023-03-01 13:27:44 +0800

    m

--- review/79.单词搜索.java
+++ review/79.单词搜索.java
@@ -9,54 +9,86 @@ import java.util.Arrays;
 
 // @lc code=start
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        int m=board.length,n=board[0].length;
-        boolean[][] used=new boolean[m][n];
-        if(m==1&&n==1&&word.charAt(0)==board[0][0]){
-            return true;
-        }
-        for(int i=0;i<m;i++){
-            Arrays.fill(used[i], false);
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(dfs(board,word,i,j,"",used)){
-                    return true;
-                }
+        public boolean exist(char[][] board, String word) {
+            int m=board.length,n=board[0].length;
+            boolean[][] used=new boolean[m][n];
+            for(int i=0;i<m;i++){
+                Arrays.fill(used[i], false);
             }
-        }
-        return false;
-    }
-    boolean dfs(char[][] board, String word,int i,int j,String tmp,boolean[][] used){
-        if(tmp.length()>word.length()){
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(board[i][j]==word.charAt(0)){
+                        if(dfs(board, word, i, j, 0, used)){
+                        return true;
+                        }
+                    }
+                }  
+            }
             return false;
         }
-        if(tmp.equals(word)){
-            return true;
+        boolean dfs(char[][] board, String word,int i,int j,int index,boolean[][] used){
+
+            if(index==word.length()){
+                return true;
+            }
+            if (i < 0 || j < 0 || i >= board.length || j >= board[i].length) {
+                return false;
+            }
+            if(word.charAt(index)!=board[i][j]){
+                return false;
+            }
+            char t=board[i][j];
+            board[i][j]='0';
+            boolean res=dfs(board, word, i-1, j, index+1,used)
+            ||dfs(board, word, i, j-1,index+1 ,used)
+            ||dfs(board, word, i+1, j,index+1,used)
+            ||dfs(board, word, i, j+1, index+1,used);
+            board[i][j]=t;
+            return res;
         }
-        boolean f1=false,f2=false,f3=false,f4=false;
-        if(i>1&&!used[i-1][j]){
-            used[i][j]=true;
-            f1=dfs(board, word, i-1, j, tmp+board[i][j],used);
-            used[i][j]=false;
-        }
-        if(j>1&&!used[i][j-1]){
-            used[i][j]=true;
-            f2=dfs(board, word, i, j-1, tmp+board[i][j],used);
-            used[i][j]=false;
-        }
-        if(i<board.length-1&&!used[i+1][j]){
-            used[i][j]=true;
-            f3=dfs(board, word, i+1, j, tmp+board[i][j],used);
-            used[i][j]=false;
-        }
-        if(j<board[0].length-1&&!used[i][j+1]){
-            used[i][j]=true;
-            f4=dfs(board, word, i, j+1, tmp+board[i][j],used);
-            used[i][j]=false;
-        }
-        return f1||f2||f3||f4;
     }
-}
 // @lc code=end
 
+// Soulution1:  wrong
+// class Solution {
+//     public boolean exist(char[][] board, String word) {
+//         int m=board.length,n=board[0].length;
+//         boolean[][] used=new boolean[m][n];
+//         for(int i=0;i<m;i++){
+//             Arrays.fill(used[i], false);
+//         }
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(dfs(board, word, i, j, "", used)){
+//                     return true;
+//                 }
+//             }  
+//         }
+//         return false;
+//     }
+//     boolean dfs(char[][] board, String word,int i,int j,String tmp,boolean[][] used){
+//         if(tmp.length()>word.length()){
+//             return false;
+//         }
+//         if(tmp.equals(word)){
+//             return true;
+//         }
+//         boolean f1=false,f2=false,f3=false,f4=false;
+//         if (i < 0 || j < 0 || i >= board.length || j >= board[i].length) {
+//             return false;
+//         }
+//         used[i][j]=true;
+//         f1=dfs(board, word, i-1, j, tmp+board[i][j],used);
+//         used[i][j]=false;
+//         used[i][j]=true;
+//         f2=dfs(board, word, i, j-1, tmp+board[i][j],used);
+//         used[i][j]=false;
+//         used[i][j]=true;
+//         f3=dfs(board, word, i+1, j, tmp+board[i][j],used);
+//         used[i][j]=false;
+//         used[i][j]=true;
+//         f4=dfs(board, word, i, j+1, tmp+board[i][j],used);
+//         used[i][j]=false;
+//         return f1||f2||f3||f4;
+//     }
+// }
